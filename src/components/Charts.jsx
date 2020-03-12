53014d6e38c5d2811fd9fa7dd7bd4dc0a344295b
Anthony Kawa <anthonyk2020@gmail.com>
2020-03-11 23:29:31 -0700

    stretch goals

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Chart from "./Chart";
 
-const Charts = ({ coinData }) => {
+const Charts = ({ coinData, currentCoinData }) => {
   return (
     <div className="charts">
-      {coinData.map(coin => (
-        <div className="chart__container" key={coin.name}>
-          <h2 className="coin__title">{coin.name}</h2>
-          <h4 className="coin__symbol">{coin.symbol}</h4>
+      {/* {coinData.map(coin => ( */}
+      {currentCoinData ? 
+        <div className="chart__container" key={currentCoinData.name}>
+          <h2 className="coin__title">{currentCoinData.name}</h2>
+          <h4 className="coin__symbol">{currentCoinData.symbol}</h4>
           <div className="coin__logo">
-            <img src={coin.image} height="40" alt={coin.name} />
+            <img src={currentCoinData.image} height="40" alt={currentCoinData.name} />
           </div>
-          <Chart sparklineData={coin.sparkline_in_7d.price} />
+          <Chart sparklineData={currentCoinData.sparkline_in_7d.price} />
         </div>
-      ))}
+        : null} 
+      {/* ))} */}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,24 +4,28 @@ import axios from "axios";
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
+import Dropdown from './components/Dropdown';
 
 import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]); 
+  const [currentCoinData, setCurrentCoinData] = useState();
 
   useEffect(() => {
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
+      .then(res => {
+        setCoinData(res.data)})
       .catch(err => console.log(err));
   }, []);
   return (
     <div className='App'>
       <Navbar />
-      <Charts coinData={coinData} />
+      <Dropdown coinData={coinData} setCurrentCoinData={setCurrentCoinData} />
+      <Charts coinData={coinData} currentCoinData={currentCoinData} />
     </div>
   );
 };
